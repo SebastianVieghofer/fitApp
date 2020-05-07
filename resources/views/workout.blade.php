31a9883631428a1c421bc086aacb1bbdaa2dfe0b
Sebastian <sebastian3270@gmail.com>
2020-05-07 10:23:26 +0200

    Show Streak Explanation before WO starts.

--- resources/views/workout.blade.php
+++ resources/views/workout.blade.php
@@ -12,6 +12,10 @@
         <button type="button" onclick="countDown();" style="margin-bottom:20px;" id="WoStartButton">Starte dein Workout</button>
         <div id="displayNoneWhenButtonClicked">Für manche der Übungen brauchst du ein Handtuch.</div> 
         <div id="displayNoneWhenButtonClicked3">Für jede Übung hast du <span class="highlightTeamData">2 Minuten</span> Zeit.</div> 
+        <details id="displayNoneWhenButtonClicked4">
+            <summary>Was ist ein Streak?</summary>
+            <div class="MargingBottom20px">Mache dein nächstes Workout innerhalb von 2 Tagen und dein Streak wird steigen! Für jede Trainingseinheit bekommst du Punkte, welche mit deinem Streak multipliziert werden.</div>
+        </details>
         <div id="displayNoneWhenButtonClicked2">Diese Übungen erwarten dich:</div> 
     </div>
 
@@ -89,6 +93,7 @@
         document.getElementById("displayNoneWhenButtonClicked").style.display = "none";
         document.getElementById("displayNoneWhenButtonClicked2").style.display = "none";
         document.getElementById("displayNoneWhenButtonClicked3").style.display = "none";
+        document.getElementById("displayNoneWhenButtonClicked4").style.display = "none";
         document.getElementById("clock").style.display = "block";
         //document.getElementById("push").style.display = "none";
         document.getElementById("leg").style.display = "none";
